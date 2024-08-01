7abff5c3d18f25edf6857e6ea885d8c7529efed6
Shahidhameed244 <102786927+Shahidhameed244@users.noreply.github.com>
2024-08-01 18:04:55 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Owin.Security;
+using Microsoft.Owin.Security.Cookies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Google.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IAuthenticationManager AuthenticationManager => HttpContext.GetOwinContext().Authentication;
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string returnUrl)
+        {
+            return new ChallengeResult("Google", Url.Action("ExternalLoginCallback", "Home", new { ReturnUrl = returnUrl }));
+        }
+
+        public ActionResult ExternalLoginCallback(string returnUrl)
+        {
+            var loginInfo = AuthenticationManager.GetExternalLoginInfo();
+            if (loginInfo == null)
+            {
+                return RedirectToAction("Login");
+            }
+
+            // Retrieve claims from the external login info
+            var claimsIdentity = (ClaimsIdentity)loginInfo.ExternalIdentity;
+            var claims = claimsIdentity.Claims.Select(claim => new
+            {
+                claim.Issuer,
+                claim.OriginalIssuer,
+                claim.Type,
+                claim.Value
+            });
+
+            return Json(claims, JsonRequestBehavior.AllowGet);
+        }
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+          
+            return RedirectToAction("Index", "Home",returnUrl);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    internal class ChallengeResult : HttpUnauthorizedResult
+    {
+        public ChallengeResult(string provider, string redirectUri)
+        {
+            LoginProvider = provider;
+            RedirectUri = redirectUri;
+        }
+
+        public string LoginProvider { get; set; }
+        public string RedirectUri { get; set; }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            var properties = new AuthenticationProperties { RedirectUri = RedirectUri };
+            context.HttpContext.GetOwinContext().Authentication.Challenge(properties, LoginProvider);
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin.Security.Google;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Google.Startup))]
+namespace Google
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login")
+            });
+
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions
+            {
+                ClientId = "yourid",
+                ClientSecret = "yourid",
+                CallbackPath = new PathString("/signin-google")
+            });
+        }
+    }
+}
+
+
